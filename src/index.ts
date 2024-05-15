546aadc34d7c40bbfa34b2aaf1f5d55163d76fda
Luiz Felipe <111662576+LuizORAS@users.noreply.github.com>
2024-05-15 17:59:26 -0300

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,95 @@
+interface GithubUserResponse {
+    id: number
+    login: string
+    name: string 
+    bio: string
+    public_repos: number
+    repos_url: string
+    message?: "Not Found"
+}
+
+interface GithubRepoResponse {
+    name: string
+    description: string
+    fork: boolean
+    stargazers_count: number
+}
+
+const users: GithubUserResponse[] = []
+
+async function fetchUser(userName:string) {
+    const response = await fetch(`https://api.github.com/users/${userName}`)
+    const user: GithubUserResponse = await response.json()
+
+    if(user.message){
+        console.log("USUÁRIO NÃO ENCONTRADO")
+    }else{
+        users.push(user)
+        console.log(
+            `O usuário ${user.login} foi salvo.\n` +
+            `\nid: ${user.id}` +
+            `\nlogin: ${user.login}` +
+            `\nNome: ${user.name}` +
+            `\nBio: ${user.bio}` +
+            `\nRepositórios públicos: ${user.public_repos}`
+        )
+    }
+
+}
+
+async function showUser (userName: string){
+
+    const user = users.find( user => user.login === userName)
+
+    if(typeof user === 'undefined'){
+        console.log('USUÁRIO NÃO ENCONTRADO!')
+    }else{
+        const response = await fetch(user.repos_url)
+        const repos: GithubRepoResponse[] = await response.json()
+
+        let message = `id: ${user.id}\n` +
+        `\nlogin: ${user.login}` +
+        `\nNome: ${user.name}` +
+        `\nBio: ${user.bio}` +
+        `\nRepositórios públicos: ${user.public_repos}`
+  
+      repos.forEach(repo => {
+        message += `\nNome: ${repo.name}` +
+          `\nDescrição: ${repo.description}` +
+          `\nEstrelas: ${repo.stargazers_count}` +
+          `\nÉ um fork: ${repo.fork ? 'Sim' : 'Não'}\n`
+      })
+
+      console.log(message)
+    }
+
+}
+
+function showAllUsers (){
+    let message = `Usuários:\n`
+
+    users.forEach( (user) => {
+        message += `\n=> ${user.login}`
+    })
+    console.log(message)
+}
+
+function showAllRepos (){
+    const reposTotal = users.reduce( (accum, user) => accum + user.public_repos, 0)
+
+    console.log(`O grupo possui um total de ${reposTotal} repositórios públicos!`)
+}
+
+function showTopFive (){
+    
+    const TopFive = users.slice().sort( (a,b) => b.public_repos - a.public_repos).slice(0,5)
+
+    let message = "Top Usuários com mais repositórios públicos: \n"
+
+    TopFive.forEach((user, index) => {
+        message += `\n${index + 1} - ${user.login}: ${user.public_repos} repositórios`
+      }
+    )
+    
+    console.log(message)
+}
